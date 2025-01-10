f712581d7eee0881b976ca6bf15dafdee165400e
juanlopcon07 <juan.21futmas07@gmail.com>
2025-01-09 21:31:05 -0500

    API

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,16 +1,16 @@
-const Card = () => {
+const Card = (data) => {
     return (
         <div className='w-56 bg-white rounded-lg cursor-pointer h-60'>
             <figure className='relative w-full mb-2 h-4/5'>
-                <span className='absolute bottom-0 left-0 m-2 text-xs text-black rounded-lg bg-white/60 px-3 py-0.5'>Electronics</span>
-                <img className='object-cover w-full h-full rounded-lg' src='https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='headphones' />
+                <span className='absolute bottom-0 left-0 m-2 text-xs text-black rounded-lg bg-white/60 px-3 py-0.5'>{data.data.category.name}</span>
+                <img className='object-cover w-full h-full rounded-lg' src={data.data.images[0]} alt={data.data.title} />
                 <div className='absolute top-0 right-0 flex items-center justify-center w-6 h-6 p-1 m-2 bg-white rounded-full'>
                     +
                 </div>
             </figure>
             <p className='flex justify-between'>
-                <span className='text-sm font-light'>Headphones</span>
-                <span className='text-lg font-medium'>$300</span>
+                <span className='text-sm font-light'>{data.data.title}</span>
+                <span className='text-lg font-medium'>${data.data.price}</span>
             </p>
         </div>
     )
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,26 @@
+import { useEffect, useState } from "react"
 import Layout from "../../Components/Layout"
 import Card from "../../Components/Card"
 
 function Home() {
+    const [items, setItems] = useState(null)
 
+    useEffect(() => {
+      fetch('https://api.escuelajs.co/api/v1/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+      
+    },[])
     return (
       <Layout>
         Home 
-        <Card/>
+        <div className='grid w-full max-w-screen-lg grid-cols-4 gap-4'>
+        {
+          items?.map(item => (
+            <Card key={item.id} data={item}/>
+          ))
+        }
+        </div>
       </Layout>
     )
   }
