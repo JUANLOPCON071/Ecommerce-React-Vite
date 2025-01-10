8968b877e10b9dd398e0d249135046d98aeba4e4
juanlopcon07 <juan.21futmas07@gmail.com>
2025-01-09 22:47:08 -0500

    Detalle del producto

---
+++ src/Components/ProductDetail/index.jsx
@@ -0,0 +1,14 @@
+import './styles.css'
+
+const ProductDetail = () => {
+    return (
+        <aside className='fixed right-0 flex flex-col bg-white border border-black rounded-lg product-detail'>
+            <div className='flex items-center justify-between p-6'>
+                <h2 className='text-xl font-medium'>Detail</h2>
+                <div>x</div>
+            </div>
+        </aside>
+    )
+}
+
+export default ProductDetail;
