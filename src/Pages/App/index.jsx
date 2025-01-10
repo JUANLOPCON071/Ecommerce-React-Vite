4eee78819044f7bab2628e717bde24be8d21a788
juanlopcon07 <juan.21futmas07@gmail.com>
2025-01-09 22:02:16 -0500

    Context

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -7,6 +7,7 @@ import NotFound from '../NotFound'
 import SignIn from '../SignIn'
 import Navbar from '../../Components/NavBar'
 import './App.css'
+import { ShoppingCartProvider } from '../../Components/Context'
 
 const AppRoutes = () => {
   let routes = useRoutes([
@@ -23,10 +24,12 @@ const AppRoutes = () => {
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes/>
-      <Navbar/>
-    </BrowserRouter>
+    <ShoppingCartProvider>
+      <BrowserRouter>
+        <AppRoutes/>
+        <Navbar/>
+      </BrowserRouter>
+    </ShoppingCartProvider>
   )
 }
 
