dea60c40bb61a852bae8236daf248b1a899c9ffe
juanlopcon07 <juan.21futmas07@gmail.com>
2025-01-14 21:43:15 -0500

    Evitar productos duplicados

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { PlusIcon } from '@heroicons/react/24/solid'
+import { PlusIcon, CheckIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from "../../Context";
 
 const Card = (data) => {
@@ -16,8 +16,30 @@ const Card = (data) => {
         context.setCartProducts([...context.cartProducts, productData])
         context.openCheckoutSideMenu()
         context.closeProductDetail()
-        console.log('CART:', context.cartProducts);
+        // console.log('CART:', context.cartProducts);
         
+    }
+
+    const renderIcon = (id) => {
+        const isInCart = context.cartProducts.filter(product => product.id === id).length > 0
+
+        if(isInCart) {
+            return (
+                <div 
+                  className='absolute top-0 right-0 flex items-center justify-center w-6 h-6 p-1 m-2 bg-black rounded-full'
+                >
+                    <CheckIcon className='w-6 h-6 text-white'></CheckIcon>
+                </div>
+            )
+        } else {
+            return (
+                <div 
+                className='absolute top-0 right-0 flex items-center justify-center w-6 h-6 p-1 m-2 bg-white rounded-full'
+                onClick={(event) => addProductToCart(event,data.data)}>
+                <PlusIcon className='w-6 h-6 text-black'></PlusIcon>
+            </div>
+            )
+        }
     }
     return (
         <div 
@@ -26,11 +48,7 @@ const Card = (data) => {
             <figure className='relative w-full mb-2 h-4/5'>
                 <span className='absolute bottom-0 left-0 m-2 text-xs text-black rounded-lg bg-white/60 px-3 py-0.5'>{data.data.category.name}</span>
                 <img className='object-cover w-full h-full rounded-lg' src={data.data.images[0]} alt={data.data.title} />
-                <div 
-                  className='absolute top-0 right-0 flex items-center justify-center w-6 h-6 p-1 m-2 bg-white rounded-full'
-                  onClick={(event) => addProductToCart(event,data.data)}>
-                    <PlusIcon className='w-6 h-6 text-black'></PlusIcon>
-                </div>
+                {renderIcon(data.data.id)}
             </figure>
             <p className='flex justify-between'>
                 <span className='text-sm font-light'>{data.data.title}</span>
