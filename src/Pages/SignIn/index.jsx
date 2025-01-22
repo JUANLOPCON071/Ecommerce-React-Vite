56dfc5c189ba1e7475968444ba1b37acfde02bd2
juanlopcon07 <juan.21futmas07@gmail.com>
2025-01-22 13:55:56 -0500

    SignIn y renderizado condicional del navbar

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,10 +1,34 @@
+import { Link } from "react-router-dom"
 import Layout from "../../Components/Layout"
 
 function SignIn() {
 
     return (
       <Layout>
-        SignIn 
+        <h1 className='mb-6 text-xl font-medium text-center w-80'>Welcome</h1> 
+        <div className='flex flex-col w-80'>
+          <p>
+            <span className='text-sm font-light'>Email:</span>
+            <span>juan@platzi.com</span>
+          </p>
+          <p>
+            <span className='text-sm font-light'>Password:</span>
+            <span>********</span>
+          </p>
+          <Link
+            to='/'>
+              <button 
+                className='w-full py-3 mt-4 mb-2 text-white bg-black rounded-lg disabled:bg-black/40'>
+                Log in
+              </button>
+          </Link>
+          <div className='text-center'>
+            <a className='text-xs font-light underline underline-offset-4' href='/'>Forgot my password</a>
+          </div>
+          <button className='py-3 mt-6 border border-black rounded-lg disabled:text-black/40 disabled:border-black/40'>
+            Sign up
+          </button>
+        </div>
       </Layout>
     )
   }
